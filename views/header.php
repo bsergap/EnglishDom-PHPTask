af8a878cef14c5040b90fad166f47e61ad1afcf1
Sergey Borodinov <root.vs.bsa@gmail.com>
2016-10-02 11:12:15 +0300

    Initial commit.

---
+++ views/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+	<meta charset="utf-8" />
+	<title>PHP Task 4 EnglishDom</title>
+	<link href="./css/layout.css?v=<?=STATICVER;?>" type="text/css" rel="stylesheet" media="all" />
+</head>
+<body>
